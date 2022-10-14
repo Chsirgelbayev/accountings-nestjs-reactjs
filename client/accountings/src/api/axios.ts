19a3b00286513f8bd12a71bb14826c0d3753e7c0
Chingiz <csirgilbaev@gmail.com>
2022-10-14 23:14:11 +0600

    Add dotenv module

--- client/accountings/src/api/axios.ts
+++ client/accountings/src/api/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+export default axios.create({
+    baseURL: process.env.REACT_APP_API_URL
 });
-
-export default instance;
